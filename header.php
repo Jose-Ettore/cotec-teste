212f216679f85fa5cdb04f333e88b6998689ba10
Guilherme Lara <54341844+Guilherme-A-Lara@users.noreply.github.com>
2020-11-19 21:02:35 -0300

    19-11-20
    Mudanças visuais

---
+++ header.php
@@ -0,0 +1,29 @@
+<header id="top">
+            <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top ">
+                <a class="navbar-brand" href="#"><img src="images/img-cotec/logo_cotec.png"> </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse mr-auto" id="navbarNavDropdown">
+                    <ul class="navbar-nav ">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="index.html">Clinica <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.html">Sobre nós </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="portfolio.html">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="services.html">Produtos</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Contato</a>
+                        </li>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
